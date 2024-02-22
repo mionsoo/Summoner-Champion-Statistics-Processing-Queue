353eee3ada7fecb8ac64c22cec62264063113c8d
Mionsoo <pooh910125@naver.com>
2024-02-22 13:17:27 +0900

    working status 처리 로직 수정

--- helper/stat_summoner.py
+++ helper/stat_summoner.py
@@ -33,25 +33,33 @@ async def wait_func(current_obj: WaitingSummonerObj, conn=None) -> int | None:
     return 1
 
 
-async def work_func(current_obj, conn) -> int | None:
-    # _conn = await connect_sql_aurora_async(RDS_INSTANCE_TYPE.READ)
-    async with conn.cursor() as cursor:
-        await cursor.execute(
-            'SELECT match_id, status '
-            'FROM b2c_summoner_match_queue '
-            f'WHERE platform_id={repr(current_obj.platform_id)} '
-            f'and puu_id={repr(current_obj.puu_id)} '
-            f'and (status != {Status.Success.code} '
-            f'and status != {Status.Error.code})'
-        )
+async def work_func(current_obj, conn):
+    async with aiohttp.ClientSession() as client:
+        result = await request_stats_async_work(current_obj, client)
 
-    not_finished_jobs = await cursor.fetchall()
-
-
-    if len(not_finished_jobs) >= 1:
+    if len(result['msg']) > 1:
         return 1
 
     return None
+    #
+    # _conn = await connect_sql_aurora_async(RDS_INSTANCE_TYPE.READ)
+    # async with conn.cursor() as cursor:
+    #     await cursor.execute(
+    #         'SELECT match_id, status '
+    #         'FROM b2c_summoner_match_queue '
+    #         f'WHERE platform_id={repr(current_obj.platform_id)} '
+    #         f'and puu_id={repr(current_obj.puu_id)} '
+    #         f'and (status != {Status.Success.code} '
+    #         f'and status != {Status.Error.code})'
+    #     )
+    #
+    # not_finished_jobs = await cursor.fetchall()
+    #
+    #
+    # if len(not_finished_jobs) >= 1:
+    #     return 1
+    #
+    # return None
 
 
 async def get_season(client):
@@ -97,3 +105,17 @@ async def request_stats(current_obj, queue_type, season, client):
         if r['msg'] != 'insert success':
             print('stats: ', r)
         return r['msg']
+
+
+async def request_stats_async_work(current_obj, client):
+    req_headers = {
+        "Referer": 'deeplol.gg'
+    }
+    url = (
+        f'https://renew.deeplol.gg/batch/stat_queue_summoner_work?'
+        f'platform_id={current_obj.platform_id}'
+        f'&puu_id={current_obj.puu_id}'
+    )
+    async with client.get(url, headers=req_headers) as response:
+        r = await response.json()
+        return r

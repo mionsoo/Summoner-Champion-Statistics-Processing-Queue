ef098d68f991645c39a486bbc830dc265db47fed
Mionsoo <pooh910125@naver.com>
2024-07-18 18:04:58 +0900

    수정

--- core/Queue/stat_match_queue.py
+++ core/Queue/stat_match_queue.py
@@ -54,11 +54,8 @@ def wrap_summoner_obj(obj: Tuple[str, str]) -> WaitingSummonerObj:
 
 class SummonerMatchQueueOperator(QueueOperator):
     async def update_incoming_data(self, conn):
-        t1 = time.time()
-        print('update_incoming_data')
         async with conn.cursor() as cursor:
             await self.add_queue(cursor, self.working_queue)
-        print(f'update_incoming_data done({time.time() - t1})')
 
     async def add_queue(self, cursor, status_obj: QueueStatus):
         if status_obj.status_type == Status.Waiting.code:

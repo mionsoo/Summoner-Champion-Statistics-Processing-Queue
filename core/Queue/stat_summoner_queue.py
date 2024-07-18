340ff0fa46c7a0a4e7bc8f3bf3c3ee9a980280f2
Mionsoo <pooh910125@naver.com>
2024-07-18 18:13:24 +0900

    수정

--- core/Queue/stat_summoner_queue.py
+++ core/Queue/stat_summoner_queue.py
@@ -96,7 +96,7 @@ class SummonerQueueOperator(QueueOperator):
         elif self.is_burst_switch_on:
             return await self.get_n_time_popped_value(self.working_queue, self.working_queue.length)
 
-        elif not self.is_burst_switch_on and self.calc_working_ratio() >= 0.8:
+        elif not self.is_burst_switch_on and self.calc_working_ratio() >= 1.0:
             self.burst_switch_on()
 
         if 1 <= self.waiting_queue.length:
